3e0287bab285bd90b43988fc78db53cc25995015
Zakary Johnson <zakary.johnson575@gmail.com>
2024-09-08 14:21:53 -0500

    added the license badge at the top of the README

---
+++ README.md
@@ -0,0 +1,42 @@
+
+
+  ![License](https://img.shields.io/badge/-MIT-blue.svg)
+
+
+## Description
+    
+    
+
+## Table of Contents (Optional)
+        
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+    
+## Installation
+    
+        
+
+## Usage
+    
+    
+    
+## Contributing
+    
+    
+    
+## License
+    
+    MIT    
+
+## Tests
+
+    
+
+## Questions
+
+    https://github.com/
+
+    If you have any additional questions please contact me at 
+    
--- index.js
+++ index.js
@@ -1,91 +1,119 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const generate = ({title,description,installation,usage,credits,license,github,email}) =>{
-    `<Your-Project-Title>
+const generate = ({
+  title,
+  description,
+  installation,
+  usage,
+  contributing,
+  license,
+  tests,
+  github,
+  email,
+}) => {
+  const README = `${title}
 
-    ## Description
-    
-    Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-    
-    - What was your motivation?
-    - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-    - What problem does it solve?
-    - What did you learn?
-    
-    ## Table of Contents (Optional)
-    
-    If your README is long, add a table of contents to make it easy for users to find what they need.
-    
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [License](#license)
-    
-    ## Installation
-    
-    What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-    
-    ## Usage
-    
-    Provide instructions and examples for use. Include screenshots as needed.
-    
-    ## Credits
-    
-    List your collaborators, if any, with links to their GitHub profiles.
-    
-    If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-    
-    If you followed tutorials, include links to those here as well.
-    
-    ## License
-    
-    The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).`
-    
-}
+  ![License](https://img.shields.io/badge/-${license}-blue.svg)
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "title",
-    message: "What is your project title?",
-  },
-  {
-    type: "input",
-    name: "description",
-    message:
-      "Provide a short description explaning the what,why, and how of your project",
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "What are the steps required to install your prject?",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "Provide instructions and examples for use",
-  },
-  {
-    type: "input",
-    name: "credits",
-    message: "List your collaborators, if any",
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "What licenses did you use?",
-    choices: ['MIT','ISC','Apache 2.0','BSD','Open Software','None' ]
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "What is your github username?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is your email?",
-  },
-]);
 
+## Description
+    
+    ${description}
+
+## Table of Contents (Optional)
+        
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+    
+## Installation
+    
+    ${installation}    
+
+## Usage
+    
+    ${usage}
+    
+## Contributing
+    
+    ${contributing}
+    
+## License
+    
+    ${license}    
+
+## Tests
+
+    ${tests}
+
+## Questions
+
+    https://github.com/${github}
+
+    If you have any additional questions please contact me at ${email}
+    `;
+  return README;
+};
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is your project title?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message:
+        "Provide a short description explaning the what,why, and how of your project",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What are the steps required to install your project?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Provide instructions and examples for use",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "List your collaborators, if any",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What licenses did you use?",
+      choices: ["MIT", "ISC", "Apache_2.0", "BSD", "Open_Software"],
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "did you perform any tests",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your github username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?",
+    },
+  ])
+  .then((answers) => {
+    if(answers.tests === 'no'){
+        answers.tests = ''
+    }
+    const readmePageContent = generate(answers);
+
+    fs.writeFile("README.md", readmePageContent, (err) =>
+      err ? console.log(err) : console.log("Successfully created README.md")
+    );
+  });
